6ec9d554d208dc97efeae451f3e6b18a882c3b06
William Young <william@werl.me>
2016-06-29 09:04:33 -0700

    Change to stack demo
    This is now a stack demo, just to see how the stack actually works.

--- boot.asm
+++ boot.asm
@@ -4,30 +4,29 @@
 
 mov ah, 0x0e ; tell the bios we want scrolling teletype mode
 
-mov al, 'H'
-int 0x10 ; tell the bios to print out the contents of `al`
-mov al, 'e'
+mov bp, 0x8000 ; Set the stack (just below the our bootsector)
+mov sp, bp ; Set the stack pointer to the beginnig of the stack
+
+push 'A' ;Note push puts 16 bits on the stack, but characters are only 8
+push 'B' ;The most significant bits will be filled with 0s
+push 'C' ;So the memory will be something like 0x00ab
+
+pop bx ;bx is a 16 bit register, hovever it has bh and bl, bl will have the last 8 bit (our character)
+mov al, bl ;So get the last 8 bits
 int 0x10
-mov al, 'l'
+
+pop bx
+mov al, bl
 int 0x10
-mov al, 'l'
+
+; Just to prove the way the stack works...
+mov al, [0x7ffe] ;16 bits away from the stack (0x8000)
 int 0x10
-mov al, 'o'
-int 0x10
-mov al, ' '
-int 0x10
-mov al, 'W'
-int 0x10
-mov al, 'o'
-int 0x10
-mov al, 'r'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'd'
-int 0x10 ;Very long hello world....
 
 jmp $ ; Infinite loop; $ means this line, so this means 'loop to this line'
+
+message:
+  db 'Booting OS',0
 
 times 510-($-$$) db 0 ;fill the 512 bit sector with 0s
 dw 0xaa55 ; Final 2 bit (magic number)
