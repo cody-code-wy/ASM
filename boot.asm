38e903ef3b596eec71c6675cef1265955fdc5d44
William Young <william@werl.me>
2016-06-29 08:19:18 -0700

    Implement hello world
    As long as we stay in 16 bit real mode the ah register is used by bios
    interrupts to choose a mode, and the al register contains the arguments
    for teh interupt. when ah=0x0e the mode is scrolling teletype, and al
    contains the character to print to screen when the interrupt 0x10
    happens.

--- boot.asm
+++ boot.asm
@@ -2,9 +2,32 @@
 ; Simple boot sector w/ teletype mode
 ;
 
-loop:
+mov ah, 0x0e ; tell the bios we want scrolling teletype mode
 
-jmp loop ; Infinite loop
+mov al, 'H'
+int 0x10 ; tell the bios to print out the contents of `al`
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'W'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, 'r'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'd'
+int 0x10 ;Very long hello world....
+
+jmp $ ; Infinite loop; $ means this line, so this means 'loop to this line'
 
 times 510-($-$$) db 0 ;fill the 512 bit sector with 0s
 dw 0xaa55 ; Final 2 bit (magic number)
