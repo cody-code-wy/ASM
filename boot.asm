9e0d6adaf0ae7c7b11b08194bbb166b8d6b86538
William Young <william@werl.me>
2016-06-30 10:24:11 -0700

    Slimed down hex dumping code

--- boot.asm
+++ boot.asm
@@ -7,21 +7,18 @@ mov ds, ax ;Needed for bios stuff
 mov bp, 0x8000 ; Seth the stack (just below our boot sector)
 mov sp, bp ; Set the stack pointer to the beginning of the stack
 
-mov si, msg
+mov si, hex_dump_msg
 call print_string
 
 mov si, hex_dump
 mov cx, 11
 call print_hex
 
-mov si, msg2
+mov si, ax_dump_msg
 call print_string
 
-mov dx, 0x3a6b
-call print_dx
-
-mov si, exit
-call print_string
+mov ax, 0x3a6b
+call print_ax
 
 loop:
   jmp $ ; Infinite loop; $ means this line, so this means 'loop to this line'
@@ -29,10 +26,9 @@ loop:
 %include "print_string.asm"
 %include "print_hex.asm"
 
-msg db 'Testing HEX output; should be',13,10,"|01|23|45|67|89|ab|cd|ef|00|aa|55|",13,10,"                                  <",13,0
+hex_dump_msg db 'HEX dbg',13,10,"|01|23|45|67|89|ab|cd|ef|00|aa|55|",13,10,"                                  <",13,0
 hex_dump db 0x01,0x23,0x45,0x67,0x89,0xab,0xcd,0xef,0x00,0xaa,0x55 ;This looks aweful
-msg2 db 'Testing dump data, should be',13,10,"3a6b",13,10,"    <",13,0
-exit db 'Goodbye',13,10,0
+ax_dump_msg db 'HEX a',13,10,"3a6b",13,10,"    <",13,0
 
 times 510-($-$$) db 0 ;fill the 512 bit sector with 0s
 dw 0xaa55 ; Final 2 bit (magic number)
